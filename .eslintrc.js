c57fc47bb05ca949f98081e9ce85d0e04f36f8ad
Gianfabio Pezzolla <gianfabio.pezzolla@gmail.com>
2022-03-14 23:05:31 +0100

    linter + prettier setup ready for review

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,30 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb', 'prettier'],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
+  plugins: ['prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix']
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
-        jest: true,
-      },
-    },
-  ],
+        jest: true
+      }
+    }
+  ]
 };
--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
   components: {
     Navbar,
     Contributing,
-    Footer,
-  },
+    Footer
+  }
 };
 </script>
