bb980babcb56cdd575b75d25da2c655a940c7edf
Gianfabio Pezzolla <gianfabio.pezzolla@gmail.com>
2022-03-15 22:06:34 +0100

    volta + nvm + npm strict setup ready for review

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <div class="container">
+      <div class="content" v-html="$t('pages.about')"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+<style scoped lang="scss">
+.about {
+  .content {
+    padding: 1.5em 3em;
+  }
+}
+</style>
