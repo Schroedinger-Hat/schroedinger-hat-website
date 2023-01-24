40b5efca58f4d852285fa6c5d6d107d81ffd6fac
Patrick Raedler <patoraedler@gmail.com>
2023-01-24 23:48:29 +0100

    chore(vite.config.ts): update vite config
    Vite config has been updated to support preprocessores and to resolve the directory alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: { 
+         additionalData: `@import "@/styles/global";` 
+     },
+    },
+  },
+  plugins: [vue()],
 })
