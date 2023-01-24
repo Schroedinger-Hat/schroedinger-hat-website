6242f7d0b2936317c4f432d308aee056cf210d74
Patrick Raedler <patoraedler@gmail.com>
2023-01-24 23:52:45 +0100

    chore(main.js): update and comment the file
    The main.js file has been modified to properly create a new Vue3 instance. Comments included for not yet updated deps

--- src/main.js
+++ src/main.js
@@ -4,17 +4,24 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import messages from './i18n/messages.json';
+import { createApp } from 'vue';
 
-Vue.config.productionTip = false;
-Vue.use(VueI18n);
-const i18n = new VueI18n({
-  locale: navigator.language.substr(0, 2),
-  fallbackLocale: 'en',
-  messages,
-});
+const app = createApp(App)
+app.mount('#app')
 
-new Vue({
-  router,
-  i18n,
-  render: (h) => h(App),
-}).$mount('#app');
+// Vue.config.productionTip = false;
+// Vue.use(VueI18n);
+// const i18n = new VueI18n({
+//   locale: navigator.language.substr(0, 2),
+//   fallbackLocale: 'en',
+//   messages,
+// });
+//
+// new Vue({
+//   router,
+//   i18n,
+//   render: (h) => h(App),
+// }).$mount('#app');
+
+
+
