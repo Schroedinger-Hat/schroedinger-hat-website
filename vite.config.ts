c04b2f66cfb01fa563d6d100c0fdff5378dabcaa
Patrick Raedler <patoraedler@gmail.com>
2023-01-25 16:23:58 +0100

    fix(Vuei18n): remove warning from the console
    A weird error that was appearing in the console when launching the app has been removed

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // See https://github.com/intlify/vue-i18n-next/issues/789
+      'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
     },
   },
   css: {
