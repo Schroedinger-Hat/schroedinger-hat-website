99ccb61abfb820f2339eda8f25e1c762b222bb86
Patrick Raedler <patoraedler@gmail.com>
2023-01-25 14:37:23 +0100

    fix(i18n): correctly assess user language
    Fixed an error that was preventing to assess the user browser language

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 // import VueI18n from 'vue-i18n';
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
 import messages from './i18n/messages.json'
 import './registerServiceWorker'
 
+const i18n = createI18n({
+  legacy: false,
+  locale: navigator.language.substring(0, 2),
+  fallbackLocale: 'en',
+  messages,
+})
+
 const app = createApp(App)
 
+app.use(i18n)
 app.use(router)
 app.mount('#app')
 
