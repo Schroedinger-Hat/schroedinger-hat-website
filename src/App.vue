ffe9a5349dd61e3df5625d4465ab20d8208c81f6
Patrick Raedler <patoraedler@gmail.com>
2023-01-25 14:35:28 +0100

    fix(style): repair scss import for whole app
    By an erroneous deletion the styles werent injecting into the app. Now that has been fixed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import TheNavbar from './components/layout/TheNavbar.vue';
-import TheContributing from './components/TheContributing.vue';
-import TheFooter from './components/layout/TheFooter.vue';
+import TheNavbar from './components/layout/TheNavbar.vue'
+import TheContributing from './components/TheContributing.vue'
+import TheFooter from './components/layout/TheFooter.vue'
 </script>
 
 <template>
-  <div id="app">
-    <TheNavbar />
-    <router-view />
-    <TheContributing />
-    <TheFooter />
-  </div>
+  <TheNavbar />
+  <router-view />
+  <TheContributing />
+  <TheFooter />
 </template>
 
+<style lang="scss">
+/* app */
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   css: {
     preprocessorOptions: {
-      scss: { 
-         additionalData: `@import "@/styles/global";` 
-     },
+      scss: {
+        additionalData: '@import "@/styles/global";',
+      },
     },
   },
   plugins: [vue()],
