fc280cb4e9340bb951bf5074cde51cac2290a434
Patrick Raedler <patoraedler@gmail.com>
2023-04-01 12:56:19 +0200

    test(TheMain): create e2e tests for desktop and mobile
    The main has been testes with E2E assuring everything works correctly. Also, the structuring of the tests has also been modified in a way that will allow the sharing of environmental variables across the whole testing suites

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,35 +1,36 @@
 /// <reference types="cypress" />
 import messages from '@/i18n/messages'
 
-describe('English tests', () => {
-  describe('Tests the navbar', {
-    env: {
+describe('English tests', {
+  env: {
     // Modify with your local environment url
-      localhost: 'http://localhost:5173',
-      githubLink: 'https://github.com/Schrodinger-Hat',
-      imageGoNordLink: 'https://ign.schrodinger-hat.it',
-    },
-  }, () => {
-    beforeEach(() => {
-      // Sets the website language website to 'en_US'
-      cy.visit(Cypress.env('localhost'), {
-        onBeforeLoad(win) {
-          Object.defineProperty(win.navigator, 'language', {
-            value: 'en_US',
-          })
-        },
-      })
-      // Visit local environment and wait for mailchimp modal to appear.
-      // As the modal is loaded async, edit the time depending on your situation.
-      cy.wait(5000).then(() => {
-        cy.get('.mc-closeModal').click()
-      })
+    localhost: 'http://localhost:5173',
+    githubLink: 'https://github.com/Schrodinger-Hat',
+    imageGoNordLink: 'https://ign.schrodinger-hat.it',
+    youtube: 'https://www.youtube.com/channel/UC1QLLgrGrPmlaFhS0orykCA',
+    spotify: 'https://open.spotify.com/show/7yfkQCV6hrPIqflSqJDB2P',
+    openCollective: 'https://opencollective.com/schrodinger-hat',
+  },
+}, () => {
+  beforeEach(() => {
+    // Sets the website language website to 'en_US'
+    cy.visit(Cypress.env('localhost'), {
+      onBeforeLoad(win) {
+        Object.defineProperty(win.navigator, 'language', {
+          value: 'en_US',
+        })
+      },
     })
-    // As the mailchimp modal cannot be controlled by our code,
-    // adjust the waiting time until it closes.
-    it.skip('Changes to mobile viewport, assures all CTAs work correctly', () => {
+    // Wait for mailchimp modal to appear.
+    // As the modal is loaded async, edit the time depending on your situation.
+    cy.wait(5000).then(() => {
+      cy.get('.mc-closeModal').click()
+    })
+  })
+  describe('Navbar tests', () => {
+    it('Changes to mobile viewport, assures all CTAs work correctly', () => {
       cy.viewport('iphone-xr')
-      cy.get('[data-test="nav-wrapper"]').should('be.visible').and('not.exist')
+      cy.get('[data-test="nav-wrapper"]').should('be.visible').and('exist')
       cy.get('[data-test="nav-logo-button"]').should('be.visible').and('have.attr', 'href').and('include', '/')
       cy.get('[data-test="logo-text"]').should('be.visible').and('contain.text', 'Schrödinger Hat')
       cy.get('[data-test="nav-team-page-link"]').should('not.be.visible')
@@ -48,13 +49,31 @@ describe('English tests', () => {
       cy.get('[data-test="mobile-burger-menu-cta"]').should('exist').click()
       cy.get('[data-test="mobile-menu"]').should('not.exist')
       cy.get('[data-test="nav-burger-menu-cta"]').click()
-      // TODO Include GitHub and ImageGoNord into the messages
-      cy.get('[data-test="mobile-github-page-link"]').should('exist').and('contain.text', 'GitHub').and('have.attr', 'href', Cypress.env('githubLink'))
-      cy.get('[data-test="mobile-team-page-link"]').should('exist').and('contain.text', messages.en.navbar.team).and('have.attr', 'href', '/team')
-      cy.get('[data-test="mobile-event-page-link"]').should('exist').and('contain.text', messages.en.navbar.events).and('have.attr', 'href', '/events')
-      cy.get('[data-test="mobile-conduct-page-link"]').should('exist').and('contain.text', messages.en.navbar.codeOfConduct).and('have.attr', 'href', '/code-of-conduct')
-      cy.get('[data-test="mobile-go-nord-page-link"]').should('exist').and('contain.text', 'ImageGoNord').and('have.attr', 'href', Cypress.env('imageGoNordLink'))
-      cy.get('[data-test="mobile-homepage-link"]').should('exist').and('have.attr', 'href', '/').click()
+      // TODO: Include GitHub and ImageGoNord into the messages
+      cy.get('[data-test="mobile-github-page-link"]')
+        .should('exist')
+        .and('contain.text', 'GitHub')
+        .and('have.attr', 'href', Cypress.env('githubLink'))
+        .and('have.attr', 'target', '_blank')
+      cy.get('[data-test="mobile-team-page-link"]')
+        .should('exist')
+        .and('contain.text', messages.en.navbar.team)
+        .and('have.attr', 'href', '/team')
+      cy.get('[data-test="mobile-event-page-link"]')
+        .should('exist')
+        .and('contain.text', messages.en.navbar.events)
+        .and('have.attr', 'href', '/events')
+      cy.get('[data-test="mobile-conduct-page-link"]')
+        .should('exist')
+        .and('contain.text', messages.en.navbar.codeOfConduct)
+        .and('have.attr', 'href', '/code-of-conduct')
+      cy.get('[data-test="mobile-go-nord-page-link"]')
+        .should('exist')
+        .and('contain.text', 'ImageGoNord')
+        .and('have.attr', 'href', Cypress.env('imageGoNordLink'))
+        .and('have.attr', 'target', '_blank')
+      cy.get('[data-test="mobile-homepage-link"]').should('exist')
+        .and('have.attr', 'href', '/').click()
       cy.get('[data-test="mobile-menu"]').should('not.exist')
       cy.url().then((url) => {
         expect(url).to.contain(Cypress.env('localhost'))
@@ -66,17 +85,77 @@ describe('English tests', () => {
       cy.get('[data-test="nav-logo-button"]').should('be.visible').and('have.attr', 'href').and('include', '/')
       cy.get('[data-test="logo-text"]').should('be.visible').and('contain.text', 'Schrödinger Hat')
       cy.get('[data-test="nav-wrapper"]').should('be.visible').and('exist')
-      cy.get('[data-test="nav-team-page-link"]').should('exist').and('contain.text', messages.en.navbar.team).and('have.attr', 'href', '/team')
-      cy.get('[data-test="nav-event-page-link"]').should('exist').and('contain.text', messages.en.navbar.events).and('have.attr', 'href', '/events')
-      cy.get('[data-test="nav-conduct-page-link"]').should('exist').and('contain.text', messages.en.navbar.codeOfConduct).and('have.attr', 'href', '/code-of-conduct')
-      cy.get('[data-test="nav-go-nord-page-link"]').should('exist').and('contain.text', 'ImageGoNord').and('have.attr', 'href', Cypress.env('imageGoNordLink'))
-      cy.get('[data-test="nav-github-page-link"]').should('exist').and('have.attr', 'href', Cypress.env('githubLink'))
+      cy.get('[data-test="nav-team-page-link"]')
+        .should('exist')
+        .and('contain.text', messages.en.navbar.team)
+        .and('have.attr', 'href', '/team')
+      cy.get('[data-test="nav-event-page-link"]')
+        .should('exist')
+        .and('contain.text', messages.en.navbar.events)
+        .and('have.attr', 'href', '/events')
+      cy.get('[data-test="nav-conduct-page-link"]')
+        .should('exist')
+        .and('contain.text', messages.en.navbar.codeOfConduct)
+        .and('have.attr', 'href', '/code-of-conduct')
+      cy.get('[data-test="nav-go-nord-page-link"]')
+        .should('exist')
+        .and('contain.text', 'ImageGoNord')
+        .and('have.attr', 'href', Cypress.env('imageGoNordLink'))
+        .and('have.attr', 'target', '_blank')
+      cy.get('[data-test="nav-github-page-link"]')
+        .should('exist')
+        .and('have.attr', 'href', Cypress.env('githubLink'))
+        .and('have.attr', 'target', '_blank')
       cy.get('[data-test="nav-github-icon"]').should('have.class', 'fab fa-github').and('be.visible').and('exist')
       cy.get('html').then(($html) => {
         const oldClass = $html[0].getAttribute('class')
         cy.get('[data-test="nav-theme-cta"]').click()
         cy.get('html').should('not.have.class', oldClass)
       })
+    })
+  })
+  describe('Hero section tests', () => {
+    it('Changes to mobile viewport, assures all elements are present', () => {
+      cy.viewport('iphone-xr')
+      cy.get('[data-test="main"]').should('be.visible').and('exist')
+      cy.get('[data-test="main-h1"]').should('be.visible').and('exist').and('contain.text', messages.en.main.h1)
+      cy.get('[data-test="main-h2"]').should('be.visible').and('exist').and('contain.text', messages.en.main.h2)
+      cy.get('[data-test="main-cta-youtube"]')
+        .should('exist')
+        .and('contain.text', messages.en.main.links.youtube)
+        .and('have.attr', 'href', Cypress.env('youtube'))
+        .and('have.attr', 'target', '_blank')
+      cy.get('[data-test="main-cta-spotify"]')
+        .should('exist')
+        .and('contain.text', messages.en.main.links.spotify)
+        .and('have.attr', 'href', Cypress.env('spotify'))
+        .and('have.attr', 'target', '_blank')
+      cy.get('[data-test="main-cta-open-collective"]')
+        .should('exist')
+        .and('contain.text', messages.en.main.links.openCollective)
+        .and('have.attr', 'href', Cypress.env('openCollective'))
+        .and('have.attr', 'target', '_blank')
+    })
+    it('Changes to desktop viewport, assures all elements are present', () => {
+      cy.viewport('macbook-16')
+      cy.get('[data-test="main"]').should('be.visible').and('exist')
+      cy.get('[data-test="main-h1"]').should('be.visible').and('exist').and('contain.text', messages.en.main.h1)
+      cy.get('[data-test="main-h2"]').should('be.visible').and('exist').and('contain.text', messages.en.main.h2)
+      cy.get('[data-test="main-cta-youtube"]')
+        .should('exist')
+        .and('contain.text', messages.en.main.links.youtube)
+        .and('have.attr', 'href', Cypress.env('youtube'))
+        .and('have.attr', 'target', '_blank')
+      cy.get('[data-test="main-cta-spotify"]')
+        .should('exist')
+        .and('contain.text', messages.en.main.links.spotify)
+        .and('have.attr', 'href', Cypress.env('spotify'))
+        .and('have.attr', 'target', '_blank')
+      cy.get('[data-test="main-cta-open-collective"]')
+        .should('exist')
+        .and('contain.text', messages.en.main.links.openCollective)
+        .and('have.attr', 'href', Cypress.env('openCollective'))
+        .and('have.attr', 'target', '_blank')
     })
   })
 })
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -2,21 +2,32 @@
 </script>
 
 <template>
-  <div class="main">
+  <div data-test="main" class="main">
     <div class="middle-section">
-      <h1>{{ $t('main.h1') }}</h1>
-      <h2>{{ $t('main.h2') }}</h2>
+      <h1 data-test="main-h1">
+        {{ $t('main.h1') }}
+      </h1>
+      <h2 data-test="main-h2">
+        {{ $t('main.h2') }}
+      </h2>
       <a
+        data-test="main-cta-youtube"
         class="btn btn-primary"
         target="_blank"
         href="https://www.youtube.com/channel/UC1QLLgrGrPmlaFhS0orykCA"
       >YouTube</a>
       <a
+        data-test="main-cta-spotify"
         class="btn btn-info"
         target="_blank"
         href="https://open.spotify.com/show/7yfkQCV6hrPIqflSqJDB2P"
       >Spotify</a>
-      <a class="btn btn-info" target="_blank" href="https://opencollective.com/schrodinger-hat">OpenCollective</a>
+      <a
+        data-test="main-cta-open-collective"
+        class="btn btn-info"
+        target="_blank"
+        href="https://opencollective.com/schrodinger-hat"
+      >OpenCollective</a>
     </div>
   </div>
 </template>
