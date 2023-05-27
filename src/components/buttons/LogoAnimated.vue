ac0e93cc22f538d3b7657b35c332b4074995a591
Patrick Raedler <patoraedler@gmail.com>
2023-05-27 16:33:47 +0200

    fix(LogoAnimated): adjust position of image and text

--- src/components/buttons/LogoAnimated.vue
+++ src/components/buttons/LogoAnimated.vue
@@ -3,19 +3,19 @@
 </script>
 
 <template>
-  <div data-test="footer-logo" class="logo flex justify-center items-center py-2">
-    <img
-      data-test="footer-home-link-img"
-      alt="SH logo"
-      width="36"
-      src="@/assets/sh-logo-small.png"
-    >
-    <router-link data-test="footer-home-link" :to="{ name: 'Home' }">
-      <span data-test="footer-home-link-text" class="logo-text hidden ml-2 lg:inline">
+  <router-link data-test="footer-home-link" class="logo" :to="{ name: 'Home' }">
+    <div data-test="footer-logo" class="flex justify-center items-center">
+      <img
+        data-test="footer-home-link-img"
+        alt="SH logo"
+        width="36"
+        src="@/assets/sh-logo-small.png"
+      >
+      <span data-test="footer-home-link-text" class="logo-text hidden lg:inline ml-2">
         S<i class="part-1">chrödinger</i> H<i class="part-2">at</i>
       </span>
-    </router-link>
-  </div>
+    </div>
+  </router-link>
 </template>
 
 <style scoped lang="scss">
