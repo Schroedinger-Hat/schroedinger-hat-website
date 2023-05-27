b09fe304306c1abaeefb2c906fdfdb4c6c688b21
Patrick Raedler <patoraedler@gmail.com>
2023-05-27 18:23:07 +0200

    feat(CtaComponent): create component and function for dynamic component

---
+++ src/components/buttons/CtaComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useAttrs } from 'vue'
+import { useCtaComponent } from '../functions/useCtaComponent'
+
+const attrs = useAttrs()
+
+const { component, bindings } = useCtaComponent(attrs)
+</script>
+
+<template>
+  <component :is="component" v-bind="bindings">
+    <slot />
+  </component>
+</template>
+
+<style scoped lang="scss">
+//
+</style>
