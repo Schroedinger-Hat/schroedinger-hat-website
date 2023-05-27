278f4f82ee17667eba5948e4f42648516a33b93b
Patrick Raedler <patoraedler@gmail.com>
2023-05-27 18:56:12 +0200

    refactor(TheNavbar): simplify component

--- src/components/layout/TheNavbar.vue
+++ src/components/layout/TheNavbar.vue
@@ -1,48 +1,64 @@
 <script setup lang='ts'>
 import { computed } from 'vue'
 import { breakpointsTailwind, useBreakpoints, useDark, useToggle } from '@vueuse/core'
-import LanguageSwitcher from '../LanguageSwitcher.vue'
+import LogoAnimated from '@/components/buttons/LogoAnimated.vue'
 import MobileMenu from '@/components/layout/MobileMenu.vue'
+import CtaComponent from '@/components/buttons/CtaComponent.vue'
 
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
+// TODO: Move this data to an outer file
+// TODO We could create a component called CtaIcon based of CtaComponent
+
+const links = {
+  router: [
+    {
+      name: 'Team',
+      test: 'nav-team-page-link',
+      text: 'navbar.team',
+    },
+    {
+      name: 'EventList',
+      test: 'nav-team-page-link',
+      text: 'navbar.events',
+    },
+    {
+      name: 'CodeOfConduct',
+      test: 'nav-team-page-link',
+      text: 'navbar.codeOfConduct',
+    },
+  ],
+}
+
+const [showMobileMenu, toggleMobileMenu] = useToggle()
 const breakpoints = useBreakpoints(breakpointsTailwind)
+const isDark = useDark()
 const smallerThanLg = breakpoints.smaller('lg')
+const toggleDark = useToggle(isDark)
 
 const themeIcon = computed(() => {
   return isDark.value ? 'fa-sun' : 'fa-moon'
 })
-
-const [showMobileMenu, toggleMobileMenu] = useToggle()
 </script>
 
 <template>
   <header class="container">
     <div class="inner-header-container" data-test="nav-wrapper">
-      <div class="logo">
-        <router-link :to="{ name: 'Home' }" data-test="nav-logo-button">
-          <img alt="SH logo" width="36" src="@/assets/sh-logo-small.png">
-          <span data-test="logo-text">S<i class="title-part-1">chrödinger</i> H<i class="title-part-2">at</i></span>
-        </router-link>
-      </div>
+      <LogoAnimated />
       <nav>
         <div class="navbar" data-test="nav-link-wrapper">
-          <router-link :to="{ name: 'Team' }" data-test="nav-team-page-link">
-            {{ $t('navbar.team') }}
-          </router-link>
-          <router-link :to="{ name: 'EventList' }" data-test="nav-event-page-link">
-            {{ $t('navbar.events') }}
-          </router-link>
-          <router-link :to="{ name: 'CodeOfConduct' }" data-test="nav-conduct-page-link">
-            {{ $t('navbar.codeOfConduct') }}
-          </router-link>
+          <CtaComponent
+            v-for="{ test, text, name } in links.router"
+            :key="text"
+            :to="{ name }"
+            :data-test="test"
+          >
+            {{ $t(text) }}
+          </CtaComponent>
           <a
             id="gonord" href="https://ign.schrodinger-hat.it" target="_blank"
             data-test="nav-go-nord-page-link"
           >
             ImageGoNord
           </a>
-          <LanguageSwitcher />
           <a href="https://github.com/Schrodinger-Hat" target="_blank" data-test="nav-github-page-link">
             <i class="fab fa-github" data-test="nav-github-icon" />
           </a>
@@ -66,78 +82,38 @@ const [showMobileMenu, toggleMobileMenu] = useToggle()
 
 <style scoped lang="scss">
 header {
-  margin: auto;
-  text-align: left;
-  height: 5em;
   position: relative;
   z-index: $z-header;
   display: flex;
+  height: 5em;
   padding: 0 !important;
+  margin: auto;
+  text-align: left;
 
   .inner-header-container {
-    width: 100%;
     display: flex;
-    justify-content: space-between;
-    -webkit-box-align: center;
+    width: 100%;
     align-items: center;
-    -webkit-box-pack: justify;
-    margin: 0px auto;
+    justify-content: space-between;
     padding: 0 0.5em;
-
-    .logo {
-      display: flex;
-      -webkit-box-align: center;
-      align-items: center;
-
-      span {
-        font-size: 1.8em;
-        font-weight: 600;
-        margin-left: 0.5em;
-        vertical-align: super;
-        transition: opacity 200ms ease-in-out 0s;
-
-        i {
-          position: absolute;
-          z-index: -1;
-          opacity: 0;
-          transition: all 300ms ease-in-out 0s;
-        }
-
-        .title-part-1 {
-          margin-left: -90px;
-        }
-
-        .title-part-2 {
-          margin-left: 90px;
-        }
-
-        &:hover {
-          i {
-            position: relative;
-            z-index: 0;
-            opacity: 1;
-            font-style: normal;
-            margin-left: 0;
-            transition: all 300ms ease-in-out 0s;
-          }
-        }
-      }
-    }
+    margin: 0 auto;
+    -webkit-box-align: center;
+    -webkit-box-pack: justify;
 
     nav {
       display: flex;
 
       .navbar {
-        list-style: none;
-        -webkit-box-pack: justify;
         justify-content: space-between;
+        -webkit-box-pack: justify;
+        list-style: none;
 
         a {
           border-radius: 0.25em;
           margin: 0 0.4em;
-          transition: background-color 100ms ease-in-out 0s;
           cursor: pointer;
           font-size: 1.2em;
+          transition: background-color 100ms ease-in-out 0s;
 
           &:nth-child(-n + 3) {
             display: none;
@@ -149,13 +125,13 @@ header {
         }
 
         button {
-          border-radius: 0.25em;
-          background-color: transparent;
           border: none;
+          border-radius: 0.25em;
           margin: 0 0.4em;
-          transition: background-color 100ms ease-in-out 0s;
+          background-color: transparent;
           cursor: pointer;
           font-size: 1.2em;
+          transition: background-color 100ms ease-in-out 0s;
 
           &:nth-child(-n + 3) {
             display: none;
@@ -170,10 +146,10 @@ header {
   }
 }
 
-@media (min-width: 56.25em) {
+@media (width >= 56.25em) {
   header {
     .inner-header-container {
-      padding: 0 0;
+      padding: 0;
 
       .logo {
         span {
@@ -198,7 +174,7 @@ header {
   }
 }
 
-@media (max-width: 900px) {
+@media (width <= 900px) {
   .logo span {
     pointer-events: none;
   }
@@ -213,7 +189,6 @@ header {
     .inner-header-container {
       nav {
         .navbar {
-
           a,
           button {
             &:hover {
