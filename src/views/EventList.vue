70ec1010ade0d6a0d5139c0c1ead09edc56457a1
Patrick Raedler <patoraedler@gmail.com>
2023-06-03 18:35:02 +0200

    style(EventList): remove scss styles, add unocss styles

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -5,42 +5,43 @@ const events = Object.keys(messages.it.events)
 </script>
 
 <template>
-  <div class="eventlist">
-    <div class="container">
-      <div class="content">
-        <div class="headline">
-          <h1 data-test="events-header">
-            {{ $t(`navbar.events`) }}
-          </h1>
-        </div>
-        <router-link
-          v-for="event in events"
-          :key="event"
-          :data-test="`event-${event}-link`"
-          :to="{ name: 'EventView', params: { event } }"
-        >
-          <div class="blog-card">
-            <div class="meta">
-              <div
-                :data-test="`event-${event}-photo`"
-                class="photo"
-                :style="`background-image: url( ${$t(`events.${event}.image`)} );`"
-              />
-            </div>
-            <div class="description">
+  <div class="event-list flex flex-col justify-center items-center container">
+    <div>
+      <h1 class="head-3 text-left self-start" data-test="events-header">
+        {{ $t(`navbar.events`) }}
+      </h1>
+      <router-link
+        v-for="event in events"
+        :key="event"
+        :data-test="`event-${event}-link`"
+        :to="{ name: 'EventView', params: { event } }"
+        class="block max-w-[700px] my-2 shadow-md"
+      >
+        <div class="blog-card flex flex-col md:flex-row rounded">
+          <div class="meta overflow-hidden relative h-50 basis-2/5 md:h-auto">
+            <div
+              :data-test="`event-${event}-photo`"
+              class="photo absolute inset-0 bg-center bg-cover"
+              :style="`background-image: url( ${$t(`events.${event}.image`)} );`"
+            />
+          </div>
+          <div class="basis-3/5 w-full h-full z-1">
+            <div class="description p-4 relative space-y-5">
               <h1
-
+                class="h4"
                 :data-test="`event-${event}-title`"
               >
                 {{ $t(`events.${event}.title`) }}
               </h1>
               <h2
+                class="mt-1 font-light"
                 :data-test="`event-${event}-date`"
               >
                 {{ $t(`events.${event}.date`) }} | {{ $t(`events.${event}.location`) }}
               </h2>
               <p
                 :data-test="`event-${event}-subtitle`"
+                class="mt-1"
               >
                 {{ $t(`events.${event}.subtitle`) }}
                 <br><br>
@@ -50,14 +51,14 @@ const events = Object.keys(messages.it.events)
               </div> -->
               <p
                 :data-test="`event-${event}-read-more`"
-                class="read-more"
+                class="mt-1 text-right"
               >
                 {{ $t(`message.common.read-more`) }}
               </p>
             </div>
           </div>
-        </router-link>
-      </div>
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
@@ -68,121 +69,30 @@ $color_prime: $nord3;
 $color_grey: $nord4;
 $color_grey_dark: $nord2;
 
-.eventlist {
-  .content {
-    padding: 1.5em 3em;
-  }
-
-  .headline {
-    margin: auto;
-    max-width: 700px;
-  }
-
+.event-list {
   .blog-card {
-    display: flex;
-    flex-direction: column;
-    margin: 1rem auto;
-    box-shadow: 0 3px 7px -1px rgba(#000, 0.1);
-    margin-bottom: 1.6%;
-    background: $color_white;
-    line-height: 1.4;
-    font-family: sans-serif;
-    border-radius: 5px;
-    overflow: hidden;
-    z-index: 0;
-    a,
-    li {
-      color: $color_white;
-    }
-    &:hover {
-      .photo {
+    .photo {
+      transition: transform 0.2s;
+
+      &:hover {
         transform: scale(1.3) rotate(3deg);
       }
     }
-    .meta {
-      position: relative;
-      z-index: 0;
-      height: 200px;
-    }
-    .photo {
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      background-size: cover;
-      background-position: center;
-      transition: transform 0.2s;
-    }
 
-    .description {
-      padding: 1rem;
-      background: $color_white;
-      position: relative;
-      z-index: 1;
-      h1,
-      h2 {
-        font-family: Poppins, sans-serif;
-      }
-      h1 {
-        line-height: 1;
-        margin: 0;
-        font-size: 1.7rem;
-      }
-      h2 {
-        font-size: 1rem;
-        font-weight: 300;
-        color: $color_grey_dark;
-        margin-top: 5px;
-      }
-      .read-more {
-        text-align: right;
-        a {
-          color: $color_prime;
-          display: inline-block;
-          position: relative;
-        }
-      }
-    }
-    p {
-      position: relative;
-      margin: 1rem 0 0;
-      &:first-of-type {
-        margin-top: 1.25rem;
-      }
-    }
-
-    @media (min-width: 640px) {
-      flex-direction: row;
-      max-width: 700px;
-      .meta {
-        flex-basis: 40%;
-        height: auto;
-      }
+    @media (width >= 640px) {
       .description {
-        flex-basis: 60%;
-        &:before {
-          transform: skewX(-3deg);
-          content: '';
-          background: #fff;
-          width: 30px;
+        &::before {
           position: absolute;
-          left: -10px;
+          z-index: -1;
           top: 0;
           bottom: 0;
-          z-index: -1;
+          left: rem(-8px);
+          width: rem(32px);
+          background: transparent;
+          transform: skewX(-3deg);
         }
       }
-      &.alt {
-        flex-direction: row-reverse;
-        .description {
-          &:before {
-            left: inherit;
-            right: -10px;
-            transform: skew(3deg);
-          }
-        }
-      }
+
     }
   }
 }
@@ -190,13 +100,16 @@ $color_grey_dark: $nord2;
 .#{$dark-mode-class} {
   .blog-card {
     background: $nord2;
+
     .description {
       background: $nord2;
+
       h2,
       .read-more a {
         color: $nord4;
       }
-      &:before {
+
+      &::before {
         background: $nord2;
       }
     }
