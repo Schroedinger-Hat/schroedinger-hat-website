60e72200375d7eed7ab3ae08a265663a2b105d41
Patrick Raedler <patoraedler@gmail.com>
2023-06-17 15:59:50 +0200

    style(EventList): modify styling on eventList

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -5,60 +5,56 @@ const events = Object.keys(messages.it.events)
 </script>
 
 <template>
-  <div class="event-list flex flex-col justify-center items-center container">
-    <div>
-      <h1 class="head-3 text-left self-start" data-test="events-header">
-        {{ $t(`navbar.events`) }}
-      </h1>
-      <router-link
+  <div class="event-list w-auto mx-auto md:w-[700px]">
+    <h1 class="head-3 mb-2 text-center self-start md:text-left" data-test="events-header">
+      {{ $t(`navbar.events`) }}
+    </h1>
+    <div
+      class="w-[400px] mx-auto md:w-auto"
+    >
+      <div
         v-for="event in events"
         :key="event"
-        :data-test="`event-${event}-link`"
-        :to="{ name: 'EventView', params: { event } }"
-        class="block max-w-[700px] my-2 shadow-md"
+        class="blog-card flex flex-col mb-3 rounded shadow-lg md:flex-row md:h-[260px]"
       >
-        <div class="blog-card flex flex-col md:flex-row rounded">
-          <div class="meta overflow-hidden relative h-50 basis-2/5 md:h-auto">
-            <div
-              :data-test="`event-${event}-photo`"
-              class="photo absolute inset-0 bg-center bg-cover"
-              :style="`background-image: url( ${$t(`events.${event}.image`)} );`"
-            />
-          </div>
-          <div class="basis-3/5 w-full h-full z-1">
-            <div class="description p-4 relative space-y-5">
-              <h1
-                class="h4"
-                :data-test="`event-${event}-title`"
-              >
-                {{ $t(`events.${event}.title`) }}
-              </h1>
-              <h2
-                class="mt-1 font-light"
-                :data-test="`event-${event}-date`"
-              >
-                {{ $t(`events.${event}.date`) }} | {{ $t(`events.${event}.location`) }}
-              </h2>
-              <p
-                :data-test="`event-${event}-subtitle`"
-                class="mt-1"
-              >
-                {{ $t(`events.${event}.subtitle`) }}
-                <br><br>
-              </p>
-              <!-- <div class="sponsors">
-                <div class="logos" v-html="$t(`events.${event}.sponsors`)"></div>
-              </div> -->
-              <p
-                :data-test="`event-${event}-read-more`"
-                class="mt-1 text-right"
-              >
-                {{ $t(`message.common.read-more`) }}
-              </p>
-            </div>
+        <div class="relative h-52 overflow-hidden md:basis-2/5 md:h-auto">
+          <div
+            :data-test="`event-${event}-photo`"
+            class="photo absolute inset-0 bg-center bg-cover"
+            :style="`background-image: url( ${$t(`events.${event}.image`)} );`"
+          />
+        </div>
+        <div class="basis-3/5 w-full h-full z-1">
+          <div class="description h-full flex flex-col justify-between items-start p-3 relative space-y-2">
+            <h1
+              class="head-4 font-700"
+              :data-test="`event-${event}-title`"
+            >
+              {{ $t(`events.${event}.title`) }}
+            </h1>
+            <h2
+              class="poppins"
+              :data-test="`event-${event}-date`"
+            >
+              {{ $t(`events.${event}.date`) }} | {{ $t(`events.${event}.location`) }}
+            </h2>
+            <p
+              :data-test="`event-${event}-subtitle`"
+              class="font-bold"
+            >
+              {{ $t(`events.${event}.subtitle`) }}
+              <br><br>
+            </p>
+            <router-link
+              :data-test="`event-${event}-read-more`"
+              :to="{ name: 'EventView', params: { event } }"
+              class="mt-1 self-end"
+            >
+              {{ $t(`message.common.read-more`) }}
+            </router-link>
           </div>
         </div>
-      </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -79,21 +75,6 @@ $color_grey_dark: $nord2;
       }
     }
 
-    @media (width >= 640px) {
-      .description {
-        &::before {
-          position: absolute;
-          z-index: -1;
-          top: 0;
-          bottom: 0;
-          left: rem(-8px);
-          width: rem(32px);
-          background: transparent;
-          transform: skewX(-3deg);
-        }
-      }
-
-    }
   }
 }
 
@@ -112,6 +93,21 @@ $color_grey_dark: $nord2;
       &::before {
         background: $nord2;
       }
+    }
+  }
+}
+
+@media (width >= 640px) {
+  .description {
+    &::before {
+      position: absolute;
+      z-index: -1;
+      top: 0;
+      bottom: 0;
+      left: rem(-8px);
+      width: rem(32px);
+      background: transparent;
+      transform: skewX(-3deg);
     }
   }
 }
