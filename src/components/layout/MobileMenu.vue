13c86d513a0b243761509ba5ee6f568a4b5a10ac
Patrick Raedler <patoraedler@gmail.com>
2023-08-22 00:00:51 +0200

    refactor(MobileMenu): convert to UnoCSS when possible

--- src/components/layout/MobileMenu.vue
+++ src/components/layout/MobileMenu.vue
@@ -4,100 +4,106 @@ const props = defineProps<{
   smallerThanLg: boolean
 }>()
 
-const emit = defineEmits<{
+defineEmits<{
   (e: 'onCloseMenu'): void
 }>()
+
+const links = [
+  {
+    id: 'Team',
+    test: 'team-link',
+    text: 'navbar.team',
+    to: 'Team',
+  },
+  {
+    id: 'Events',
+    test: 'events-link',
+    text: 'navbar.events',
+    to: 'EventList',
+  },
+  {
+    id: 'CodeOfConduct',
+    test: 'conduct-link',
+    text: 'navbar.codeOfConduct',
+    to: 'CodeOfConduct',
+  },
+  {
+    href: 'https://github.com/Schrodinger-Hat',
+    id: 'Github',
+    test: 'github-link',
+    text: 'GitHub',
+  },
+  {
+    href: 'https://ign.schrodinger-hat.it',
+    id: 'IGN',
+    test: 'ign-link',
+    text: 'ImageGoNord',
+  },
+]
 </script>
 
 <template>
   <transition name="slide">
-    <div v-if="props.showMobileMenu && props.smallerThanLg" class="mobile-menu-container">
-      <div class="mobile-menu-header">
-        <div class="logo">
-          <router-link
-            data-test="mobile-homepage-link"
-            :to="{ name: 'Home' }"
-            @click="emit('onCloseMenu')"
-          >
-            <img alt="SH logo" width="36" src="@/assets/sh-logo-small.png">
-          </router-link>
-        </div>
-        <button class="close-header" data-test="mobile-burger-menu-cta" @click="emit('onCloseMenu')">
+    <div
+      v-if="props.showMobileMenu && props.smallerThanLg"
+      class="menu w-full h-[calc(100vh)] fixed left-0 z-1"
+    >
+      <div class="flex justify-between items-center p-4">
+        <RouterLink
+          data-test="mobile-homepage-link"
+          :to="{ name: 'Home' }"
+          @click="$emit('onCloseMenu')"
+        >
+          <img alt="SH logo" width="36" src="@/assets/sh-logo-small.png">
+        </RouterLink>
+        <button
+          class="my-2 border-none rounded-1 cursor-pointer"
+          data-test="mobile-burger-menu-cta"
+          @click="$emit('onCloseMenu')"
+        >
           <i class="fas fa-hamburger" />
         </button>
       </div>
-      <nav>
-        <div class="navbar" data-test="mobile-nav-link-wrapper">
-          <a
-            data-test="mobile-github-page-link"
-            href="https://github.com/Schrodinger-Hat"
-            target="_blank"
-            @click="emit('onCloseMenu')"
-          >GitHub
-          </a>
-          <router-link
-            data-test="mobile-team-page-link"
-            :to="{ name: 'Team' }"
-            @click="emit('onCloseMenu')"
+      <nav class="text-center">
+        <ul>
+          <li
+            v-for="{ href, text, to, id, test } in links"
+            :key="id"
+            class="py-4 text-xl"
           >
-            {{ $t('navbar.team') }}
-          </router-link>
-          <router-link
-            data-test="mobile-event-page-link"
-            :to="{ name: 'EventList' }"
-            @click="emit('onCloseMenu')"
-          >
-            {{ $t('navbar.events') }}
-          </router-link>
-          <router-link
-            data-test="mobile-conduct-page-link"
-            :to="{ name: 'CodeOfConduct' }"
-            @click="emit('onCloseMenu')"
-          >
-            {{ $t('navbar.codeOfConduct') }}
-          </router-link>
-          <a
-            data-test="mobile-go-nord-page-link"
-            href="https://ign.schrodinger-hat.it"
-            target="_blank"
-            @click="emit('onCloseMenu')"
-          > ImageGoNord </a>
-        </div>
+            <a
+              v-if="href" :href="href"
+              :data-test="`mobile-${test}`"
+              @click="$emit('onCloseMenu')"
+            >
+              {{ text }}
+            </a>
+            <RouterLink v-else :to="{ name: to }">
+              {{ $t(text) }}
+            </RouterLink>
+          </li>
+        </ul>
       </nav>
     </div>
   </transition>
 </template>
 
 <style scoped lang="scss">
-.mobile-menu-container {
-  position: fixed;
-  z-index: 1;
-  left: 0;
-  width: 100%;
-  height: calc(100vh);
-  background: $bg-primary;
-  transition: all 0.4s ease-in 0s;
+.menu {
+  background-color: $bg-primary;
+  transition: all 0.4s ease-in 0;
+}
 
-  nav {
-    .navbar {
-      text-align: center;
+li {
+  border-bottom: 1px solid $bg-secondary;
 
-      a {
-        display: block;
-        padding: 0.8em 0;
-        border-bottom: 1px solid $bg-secondary;
-        font-size: 1.3em;
-
-        &:last-child {
-          border-bottom: none;
-        }
-      }
-    }
+  &:first-of-type{
+    border-top: 1px solid $bg-secondary;
   }
 }
 
 .#{$dark-mode-class} {
-  .mobile-menu-container {
+  .menu {
     background: $dark-bg-primary;
   }
 }
@@ -106,33 +112,12 @@ const emit = defineEmits<{
 .slide-leave-active {
   bottom: 0;
   opacity: 1;
-  transition: bottom 0.25s ease-out 0s, opacity 0.3s ease-in-out 0s;
+  transition: bottom 0.25s ease-out 0, opacity 0.3s ease-in-out 0;
 }
 
 .slide-enter-from,
 .slide-leave-to {
   bottom: 100vh;
   opacity: 0;
-}
-
-.mobile-menu-header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1rem;
-}
-
-button {
-  border: none;
-  border-radius: 0.25em;
-  margin: 0 0.4em;
-  background-color: transparent;
-  cursor: pointer;
-  font-size: 1.2em;
-  transition: background-color 100ms ease-in-out 0s;
-
-  &:hover {
-    background-color: $bg-secondary;
-  }
 }
 </style>
