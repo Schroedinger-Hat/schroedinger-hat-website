4824065916aace16f3d2e0343e6005363ae5783d
Patrick Raedler <patoraedler@gmail.com>
2023-09-01 22:43:49 +0200

    feat(CtaIcon): create component

---
+++ src/components/buttons/CtaIcon.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useAttrs } from 'vue'
+import { useCtaComponent } from '@/functions/useCtaComponent'
+
+defineProps<{
+  icon: string
+  small?: boolean
+}>()
+
+const { component, bindings } = useCtaComponent(useAttrs())
+</script>
+
+<template>
+  <component
+    :is="component"
+    v-bind="bindings"
+    class="button mx-1 px-1 py-0.5 rounded-1"
+    :class="{ small }"
+  >
+    <i :class="icon" />
+  </component>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  font-size: rem(20px);
+
+  &.small {
+    font-size: rem(16px);
+  }
+}
+</style>
