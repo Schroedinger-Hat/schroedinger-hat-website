5746eea533a7610f7200b78d2317744aa377d6c4
Patrick Raedler <patoraedler@gmail.com>
2023-09-01 23:10:46 +0200

    refactor(TheNavbar): convert to UnoCSS, utilize CtaComponent and CtaIcon

--- src/components/layout/TheNavbar.vue
+++ src/components/layout/TheNavbar.vue
@@ -4,10 +4,10 @@ import { breakpointsTailwind, useBreakpoints, useDark, useToggle } from '@vueuse
 import LogoAnimated from '@/components/buttons/LogoAnimated.vue'
 import MobileMenu from '@/components/layout/MobileMenu.vue'
 import CtaComponent from '@/components/buttons/CtaComponent.vue'
+import CtaIcon from '@/components/buttons/CtaIcon.vue'
 
-// TODO: Throw the buttons inside the CtaComponent
 // TODO: Move this data to an outer file
-// TODO: We could create a component called CtaIcon based of CtaComponent
+// TODO: Modify
 
 const links = [
   {
@@ -24,30 +24,24 @@ const links = [
   },
   {
     id: 'CodeOfConduct',
-    to: 'CodeOfConduct',
     test: 'code-page-link',
     text: 'navbar.codeOfConduct',
+    to: 'CodeOfConduct',
   },
   {
-    id: 'IGN',
     href: 'https://ign.schrodinger-hat.it',
+    id: 'IGN',
+    target: '_blank',
     test: 'IGN-link',
-    target: '_blank',
     text: 'ImageGoNord',
-    icon: null,
-  },
-  {
-    id: 'GitHub',
-    href: 'https://github.com/Schrodinger-Hat',
-    test: 'github-link',
-    target: '_blank',
-    text: null,
-    icon: {
-      class: 'fab fa-github',
-      test: 'nav-github-icon',
-    },
   },
 ]
+
+const githubCTA = {
+  test: 'github-cta',
+  link: 'https://github.com/Schrodinger-Hat',
+  icon: 'fab fa-github',
+}
 
 const [showMobileMenu, toggleMobileMenu] = useToggle()
 const breakpoints = useBreakpoints(breakpointsTailwind)
@@ -59,139 +53,64 @@ const themeIcon = computed(() => isDark.value ? 'fa-sun' : 'fa-moon')
 </script>
 
 <template>
-  <header class="container">
+  <header class="container flex h-20 m-auto p-none z-2">
     <div class="inner-header-container flex justify-between items-center w-full mx-auto px-4 md:px-0" data-test="nav-wrapper">
       <LogoAnimated />
-      <nav>
-        <div class="navbar" data-test="nav-link-wrapper">
-          <CtaComponent
-            v-for="{ href, icon, id, target, test, text, to } in links"
-            :key="id"
-            :data-test="`data-${test}`"
-            :to="{ name: to }"
-            :href="href"
-            :target="target ? target : null"
-            class="mx-1 p-1 rounded-1 cursor-pointer text-xl md:inline"
-            :class="[{ rounded: icon, hidden: !icon }]"
-          >
-            <i v-if="icon" :class="icon.class" :data-test="icon.test" />
-            <span v-else>{{ $t(text as string) }}</span>
-          </CtaComponent>
-          <button
-            class="hamburger-none-md"
-            data-test="nav-burger-menu-cta"
-            @click="toggleMobileMenu()"
-          >
-            <i class="fas fa-hamburger" data-test="nav-hamburget-icon" />
-          </button>
-          <button
-            data-test="nav-theme-cta"
-            @click="toggleDark()"
-          >
-            <i class="fas" :class="themeIcon" data-test="nav-theme-icon" />
-          </button>
-        </div>
+      <nav class="flex">
+        <CtaComponent
+          v-for="{ id, href, to, text, test, target } in links"
+          :key="id"
+          :data-test="`data-${test}`"
+          :to="to ? { name: to } : null"
+          :href="href"
+          :target="target ? target : null"
+          class="hidden mx-1 p-1 rounded-1 cursor-pointer text-xl md:inline"
+        >
+          <span>{{ $t(text as string) }}</span>
+        </CtaComponent>
+        <CtaIcon
+          :data-test="githubCTA.test"
+          :href="githubCTA.link"
+          :icon="githubCTA.icon"
+          target="_blank"
+        />
+        <CtaIcon
+          class="md:hidden"
+          data-test="nav-burger-menu-cta"
+          icon="fas fa-hamburger"
+          @click="toggleMobileMenu()"
+        />
+        <CtaIcon
+          :icon="`fas ${themeIcon}`"
+          data-test="nav-theme-icon"
+          @click="toggleDark()"
+        />
       </nav>
     </div>
     <MobileMenu
-      data-test="mobile-menu"
       :show-mobile-menu="showMobileMenu"
       :smaller-than-lg="smallerThanLg"
+      data-test="mobile-menu"
       @on-close-menu="toggleMobileMenu()"
     />
   </header>
 </template>
 
 <style scoped lang="scss">
-header {
-  position: relative;
-  z-index: $z-header;
-  display: flex;
-  height: 5em;
-  padding: 0 !important;
-  margin: auto;
-  text-align: left;
+a:not(.logo),
+button {
+  transition: background-color 0.1s ease-in-out;
 
-    nav {
-      display: flex;
-
-      .navbar {
-        a {
-          transition: background-color 100ms ease-in-out 0s;
-
-          &:hover {
-            background-color: $bg-secondary;
-          }
-        }
-
-        button {
-          border: none;
-          border-radius: 0.25em;
-          margin: 0 0.4em;
-          background-color: transparent;
-          cursor: pointer;
-          font-size: 1.2em;
-          transition: background-color 100ms ease-in-out 0s;
-
-          &:nth-child(-n + 3) {
-            display: none;
-          }
-
-          &:hover {
-            background-color: $bg-secondary;
-          }
-        }
-      }
-    }
-  }
-
-@media (width >= 56.25em) {
-  header {
-      .logo {
-        span {
-          font-size: 2em;
-        }
-      }
-
-      nav {
-        .navbar {
-          .hamburger-none-md {
-            display: none;
-          }
-
-          a {
-            &:nth-child(-n + 3) {
-              display: inline;
-            }
-          }
-        }
-      }
-    }
-  }
-
-@media (width <= 900px) {
-  .logo span {
-    pointer-events: none;
-  }
-
-  #gonord {
-    display: none;
+  &:hover {
+    background-color: $bg-secondary;
   }
 }
 
 .#{$dark-mode-class} {
-  header {
-    .inner-header-container {
-      nav {
-        .navbar {
-          a,
-          button {
-            &:hover {
-              background-color: $dark-bg-secondary;
-            }
-          }
-        }
-      }
+  a:not(.logo),
+  button {
+    &:hover {
+      background-color: $dark-bg-secondary;
     }
   }
 }
