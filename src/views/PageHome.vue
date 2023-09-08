ef13a691a3d6960f0f820afb59b5f0b2907595ac
Patrick Raedler <code@readpato.dev>
2023-09-08 21:05:53 +0200

    refactor(PageHome): add CTA links

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
 import TheMain from '@/components/TheMain.vue'
+
+const links = [
+  {
+    href: 'https://open.spotify.com/show/7yfkQCV6hrPIqflSqJDB2P',
+    test: 'cta-spotify',
+    text: 'Spotify',
+  },
+  {
+    href: 'https://opencollective.com/schrodinger-hat',
+    test: 'cta-open-collective',
+    text: 'OpenCollective',
+  },
+  {
+    href: 'https://www.youtube.com/channel/UC1QLLgrGrPmlaFhS0orykCA',
+    test: 'cta-youtube',
+    text: 'YouTube',
+  },
+]
 </script>
 
 <template>
