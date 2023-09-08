d948b0fd77a6289b2c0f3ca5be0cb2f7979a9fab
Vittorio Renna <renna.vittorio@gmail.com>
2023-09-09 00:36:50 +0200

    feat: create reusable heading component for page title

---
+++ src/components/Heading.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="max-w-700px m-auto text-center">
+    <h1 class="text-2rem font-700 ">
+      <slot />
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
