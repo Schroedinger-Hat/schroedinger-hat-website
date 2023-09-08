f5b77e5a6c4ff913837a02795fbf513e31c2d4d9
Patrick Raedler <code@readpato.dev>
2023-09-08 21:00:38 +0200

    refactor(PageHome): rename component, delete style boilerplate

---
+++ src/views/PageHome.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TheMain from '@/components/TheMain.vue'
+</script>
+
+<template>
+  <main class="container">
+    <TheMain />
+  </main>
+</template>
+
+<style scoped lang="scss">
+.#{$dark-mode-class} {
+  .demo-section {
+    background: $nord2;
+  }
+}
+</style>
