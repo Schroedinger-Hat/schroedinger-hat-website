caa39f07f9ce9440e9296da990615f206747c8c8
Patrick Raedler <code@readpato.dev>
2023-09-08 21:07:09 +0200

    fix(router): add correct home component

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/views/HomeView.vue'),
+    component: () => import('@/views/PageHome.vue'),
   },
   {
     path: '/video',
