36b87ecb7e9a4b8ec6166a8b4b434ecbbc2c5ff3
Patrick Raedler <code@readpato.dev>
2023-09-09 17:15:56 +0200

    refactor(PageHome): add main content

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
-import TheMain from '@/components/TheMain.vue'
+import CtaComponent from '@/components/buttons/CtaComponent.vue'
 
 const links = [
   {
     href: 'https://open.spotify.com/show/7yfkQCV6hrPIqflSqJDB2P',
+    id: 'Spotify',
     test: 'cta-spotify',
     text: 'Spotify',
   },
   {
     href: 'https://opencollective.com/schrodinger-hat',
+    id: 'OpenCollective',
     test: 'cta-open-collective',
     text: 'OpenCollective',
   },
   {
     href: 'https://www.youtube.com/channel/UC1QLLgrGrPmlaFhS0orykCA',
+    id: 'Youtube',
     test: 'cta-youtube',
     text: 'YouTube',
   },
@@ -21,15 +24,24 @@ const links = [
 </script>
 
 <template>
-  <main class="container">
-    <TheMain />
+  <main class="flex flex-col justify-center items-center w-[80%] mx-auto text-center">
+    <h1 class="head-1 font-bold">
+      {{ $t('main.h1') }}
+    </h1>
+    <p class="head-6">
+      {{ $t('main.h2') }}
+    </p>
+    <div>
+      <CtaComponent
+        v-for="{ href, id, test, text } in links"
+        :key="id"
+        :href="href"
+        :data-test="`cta-${test}`"
+        class="btn btn-primary"
+        target="_blank"
+      >
+        {{ text }}
+      </CtaComponent>
+    </div>
   </main>
 </template>
-
-<style scoped lang="scss">
-.#{$dark-mode-class} {
-  .demo-section {
-    background: $nord2;
-  }
-}
-</style>
