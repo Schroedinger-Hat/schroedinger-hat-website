f1d0083661ba69528954caffc3d8f20aa461641b
Patrick Raedler <code@readpato.dev>
2023-09-09 18:08:42 +0200

    refactor(PageHome): add SVG and center text

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CtaComponent from '@/components/buttons/CtaComponent.vue'
+import AnimatedSVG from '@/components/AnimatedSVG.vue'
 
 const links = [
   {
@@ -24,24 +25,27 @@ const links = [
 </script>
 
 <template>
-  <main class="flex flex-col justify-center items-center w-[80%] mx-auto text-center">
-    <h1 class="head-1 font-bold">
-      {{ $t('main.h1') }}
-    </h1>
-    <p class="head-6">
-      {{ $t('main.h2') }}
-    </p>
-    <div>
-      <CtaComponent
-        v-for="{ href, id, test, text } in links"
-        :key="id"
-        :href="href"
-        :data-test="`cta-${test}`"
-        class="btn btn-primary"
-        target="_blank"
-      >
-        {{ text }}
-      </CtaComponent>
+  <main class="flex justify-center items-center w-[80%] mx-auto">
+    <AnimatedSVG />
+    <div class="ml-5">
+      <h1 class="head-1 font-bold">
+        {{ $t('main.h1') }}
+      </h1>
+      <p class="head-6">
+        {{ $t('main.h2') }}
+      </p>
+      <div>
+        <CtaComponent
+          v-for="{ href, id, test, text } in links"
+          :key="id"
+          :href="href"
+          :data-test="`cta-${test}`"
+          class="btn btn-primary"
+          target="_blank"
+        >
+          {{ text }}
+        </CtaComponent>
+      </div>
     </div>
   </main>
 </template>
