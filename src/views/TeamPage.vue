3be171793c00e0d228ec013e19c4646c913eec13
Vittorio Renna <renna.vittorio@gmail.com>
2023-09-10 15:00:39 +0200

    fix: fix h1, align to convention

--- src/views/TeamPage.vue
+++ src/views/TeamPage.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import messages from '@/i18n/messages'
 import MemberCard from '@/components/MemberCard.vue'
-import Heading from '@/components/Heading.vue'
 
 const team = Object.keys(messages.it.team)
 </script>
 
 <template>
   <div class="container">
-    <Heading data-test="team-page-headline">
+    <h1 class="max-w-700px m-auto text-center text-2rem font-700" data-test="team-page-headline">
       Schrödinger Hat's fam
-    </Heading>
+    </h1>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20px px-1.5em py-3em">
       <MemberCard
         v-for="member in team"
