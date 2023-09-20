c165a857c2d0091962155a19818b15e23e8a1c89
Vittorio Renna <renna.vittorio@gmail.com>
2023-09-20 23:29:25 +0200

    chore(config): add brand color to unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -11,4 +11,29 @@ export default defineConfig({
     // See: https://unocss.dev/presets/web-fonts
     presetWebFonts(),
   ],
+  theme: {
+    // ...
+    colors: {
+      light: {
+        text: {
+          primary: '#4c566a', // text-light-text-primary
+          secondary: '#eceff4', // text-light-text-secondary
+        },
+        bg: {
+          primary: '#eceff4', // bg-light-bg-primary
+          secondary: '#d8dee9', // bg-light-bg-secondary
+        },
+      },
+      dark: {
+        text: {
+          primary: '#eceff4', // text-dark-text-primary
+          secondary: '#4c566a', // text-dark-text-secondary
+        },
+        bg: {
+          primary: '#2e3440', // bg-dark-bg-primary
+          secondary: '#3b4252', // bg-dark-bg-secondary
+        },
+      },
+    },
+  },
 })
