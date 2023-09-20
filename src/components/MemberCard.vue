bd948059b85338410d2e8fe8a3c20a45a6cd6112
Vittorio Renna <renna.vittorio@gmail.com>
2023-09-20 23:30:30 +0200

    fix(components): align unocss classes to project, manage light-dark mode properly

--- src/components/MemberCard.vue
+++ src/components/MemberCard.vue
@@ -13,20 +13,20 @@ const { t } = useI18n()
 
 <template>
   <div
-    class="overflow-hidden rounded-5px bg-#d8dee9 dark:bg-#3b4252 p-32px font-sans"
+    class="overflow-hidden p-8 font-sans rounded bg-light-bg-secondary dark:bg-dark-bg-secondary"
     data-test="team-card"
     :data-test-member-name="`team-member-${member}`"
   >
-    <div class="relative z-0 flex h-auto items-center justify-center rounded-full mx-2em my-auto text-center">
+    <div class="relative z-0 flex h-auto items-center justify-center rounded-full my-auto text-center">
       <div
         :data-test="`team-member-${member}-index-photo`"
-        class="block w-128px h-128px rounded-full bg-center bg-cover"
+        class="block w-32 h-32 rounded-full bg-center bg-cover"
         :style="`background-image: url( ${$t(`team.${member}.image`)} );`"
       />
     </div>
-    <div class="relative z-1 flex flex-col items-center justify-center gap-4px">
+    <div class="relative z-1 flex flex-col items-center justify-center gap-1">
       <h2
-        class="text-1.5rem font-700"
+        class="text-6 font-700 text-text-primary"
         :data-test="`team-member-${member}-name`"
       >
         {{ $t(`team.${member}.name`) }}
@@ -35,7 +35,7 @@ const { t } = useI18n()
       <SocialIcons :member="member" :socials="socials" />
 
       <router-link
-        class="w-fit px-15px py-5px border-1px border-solid border-transparent rounded-full bg-#586379 text-#fff text-1rem font-700 hover:bg-#2e3440"
+        class="font-700 w-fit px-4 py-1 border-1 border-solid border-transparent rounded-full bg-light-bg-primary text-light-text-primary dark:bg-dark-bg-primary dark:text-dark-text-primary hover:bg-dark-bg-primary hover:text-dark-text-primary dark:hover:bg-light-bg-primary dark:hover:text-light-text-primary "
         :data-test="`team-member-${member}-page-link`"
         :to="{ name: 'TeamMember', params: { member } }"
       >
@@ -44,7 +44,3 @@ const { t } = useI18n()
     </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-
-</style>
