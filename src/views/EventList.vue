9e2fb2fb272ba57c44fafd09ce9709912ef7a686
Patrick Raedler <code@readpato.dev>
2023-10-16 21:57:30 +0200

    chore(EventList): clean up and comment unused stuff

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -4,11 +4,11 @@ import { useI18n } from 'vue-i18n'
 import { computed, ref } from 'vue'
 import messages from '@/i18n/messages'
 import type { EventMessageName } from '@/i18n/types'
-import type { CityType, SessionOrEventType, WeekType } from '@/i18n/events'
+import type { Category, City, TimeStatus } from '@/i18n/events'
 import { events } from '@/i18n/events'
 import FilterPill from '@/components/FilterPill.vue'
 import EventCardSm from '@/components/EventCardSm.vue'
-import EventCardLg from '@/components/EventCardLg.vue'
+import EventCard from '@/components/EventCard.vue'
 import { getWeekState } from '@/functions/getWeekState'
 
 const { t } = useI18n()
@@ -19,9 +19,9 @@ useHead({
   meta: [{ name: t('head.events.meta.name'), content: t('head.events.meta.content') }],
 })
 
-const activeCategoryFilters = ref([] as Array<SessionOrEventType>)
-const activeCityFilters = ref([] as Array<CityType>)
-const activeWeekFilters = ref([] as Array<WeekType>)
+const activeCategoryFilters = ref([] as Array<Category>)
+const activeCityFilters = ref([] as Array<City>)
+const activeWeekFilters = ref([] as Array<TimeStatus>)
 
 // const categoriesFilter = eventsMessages.map(e => e.category).filter((value, index, self) => {
 //   return self.indexOf(value) === index
@@ -33,26 +33,25 @@ const activeWeekFilters = ref([] as Array<WeekType>)
 //   return self.indexOf(value) === index
 // }) as WeekType[]
 
-const handleClickCatFilters = (filter: SessionOrEventType) => {
+const handleClickCatFilters = (filter: Category) => {
   if (!activeCategoryFilters.value.includes(filter))
     activeCategoryFilters.value.push(filter)
   else
     activeCategoryFilters.value = activeCategoryFilters.value.filter(e => e !== filter)
 }
-const handleClickCytFilters = (filter: CityType) => {
+const handleClickCytFilters = (filter: City) => {
   if (!activeCityFilters.value.includes(filter))
     activeCityFilters.value.push(filter)
   else
     activeCityFilters.value = activeCityFilters.value.filter(e => e !== filter)
 }
-const handleClickWeekFilters = (filter: WeekType) => {
+const handleClickWeekFilters = (filter: TimeStatus) => {
   if (!activeWeekFilters.value.includes(filter))
     activeWeekFilters.value.push(filter)
   else
     activeWeekFilters.value = activeWeekFilters.value.filter(e => e !== filter)
 }
 
-// TODO: sort by start date
 const filteredEvents = computed(() => {
   return events.filter((event) => {
     const categoryMatch = activeCategoryFilters.value.length === 0 || activeCategoryFilters.value.includes(event.category)
@@ -62,24 +61,17 @@ const filteredEvents = computed(() => {
   })
 })
 
-// TODO: decide logic of main card here (coming event?)
 const firstEvent = computed(() => {
   return events[0]
 })
 </script>
 
 <template>
-  <main class="w-full px-8 max-w-7xl mx-auto">
-    <!-- Title -->
-    <h1
-      class="head-3 mb-4 text-center"
-      data-test="events-header"
-    >
+  <main class="w-full p-8 max-w-7xl mx-auto">
+    <h1 class="head-3 mb-4 text-center" data-test="events-header">
       {{ $t(`navbar.sessions`) }}
     </h1>
-    <!-- Big Card -->
-    <EventCardLg :event="firstEvent" class="mb-8" />
-    <!-- Form section -->
+    <EventCard :event="firstEvent" featured class="mb-8" />
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-8 lg:gap-x-16 mb-8">
       <div class="mb-4 flex flex-col">
         <h3 class="mb-2">
