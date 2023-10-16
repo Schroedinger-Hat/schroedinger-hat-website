4f393c685404bc3166eaa97b3a7e0ee86b0d7bf3
Patrick Raedler <code@readpato.dev>
2023-10-16 21:55:58 +0200

    refactor(EventCardDetail): clean up

--- src/components/EventCardDetail.vue
+++ src/components/EventCardDetail.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+// Add SVGs with PresetIcons (carbon)
 defineProps<{
   src: string
   text: string
@@ -7,10 +8,6 @@ defineProps<{
 
 <template>
   <div class="flex gap-1 mb-1">
-    <img :src="src" alt=""> <span>{{ text }}</span>
+    <img :src="src" alt=""><span>{{ text }}</span>
   </div>
 </template>
-
-<style scoped>
-
-</style>
