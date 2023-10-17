0342236af9a91c36443c732cce580bbbc085a301
Patrick Raedler <code@readpato.dev>
2023-10-17 17:19:23 +0200

    feat(IconDetail): add new component for icon and text

---
+++ src/components/IconDetail.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  text: string
+  icon: string
+}>()
+</script>
+
+<template>
+  <div class="flex justify-center items-center">
+    <div :class="icon" class="mr-2" />
+    <span>{{ text }}</span>
+  </div>
+</template>
