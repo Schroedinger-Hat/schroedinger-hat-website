d824bad321886e601fce45b3fecc2b9d092b0d63
Patrick Raedler <code@readpato.dev>
2023-10-24 12:18:56 +0200

    refactor(EventCard): add overline field

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -14,12 +14,15 @@ defineProps<{
   >
     <div class="flex flex-col p-9 basis-2/6 space-y-3 lg:space-y-5">
       <div v-if="!featured" class="capitalize">{{ event.category }}</div>
-      <h3 class="head-5">{{ event.title }}</h3>
+      <div>
+        <h3 v-if="event.overline" class="head-5">{{ event.overline }}</h3>
+        <h3 class="head-5">{{ event.title }}</h3>
+      </div>
       <p class="text-md">{{ event.shortDescription }}</p>
-      <div class="flex-grow flex flex-col justify-center items-start">
+      <div v-if="$slots.default" class="flex-grow flex flex-col justify-center items-start">
         <slot />
       </div>
-      <div class="flex-grow">
+      <div v-if="$slots.footer" class="flex-grow">
         <slot name="footer" />
       </div>
     </div>
