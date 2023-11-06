1a18d2c53ca57b305bda602ec3a4ac2fd3211ca9
Patrick Raedler <code@readpato.dev>
2023-11-06 22:38:14 +0100

    refactor(EventForm): accept all as option value

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -8,7 +8,7 @@ const props = defineProps<{
 
 const modelCategory = defineModel<string>('modelCategory')
 const modelCity = defineModel<string>('modelCity')
-const modelDate = defineModel<string>('modelDate')
+const modelDate = defineModel<number>('modelDate')
 
 const options = readonly({
   categories: computed(() => new Set(props.events.map(event => event.category))),
@@ -23,6 +23,7 @@ const options = readonly({
       <label for="cities" class="block">Location</label>
       <select id="cities" v-model="modelCity" name="Cities" class="p-1 bg-dark-bg-secondary rounded-lg">
         <option disabled value="">Please select one...</option>
+        <option :value="undefined">All</option>
         <option v-for="city in options.cities" :key="city" :value="city">
           {{ city }}
         </option>
@@ -32,6 +33,7 @@ const options = readonly({
       <label for="categories" class="block">Category</label>
       <select id="categories" v-model="modelCategory" name="Categories" class="capitalize p-1 bg-dark-bg-secondary">
         <option disabled value="">Please select one...</option>
+        <option :value="undefined">All</option>
         <option v-for="category in options.categories" :key="category" :value="category">
           {{ category }}
         </option>
@@ -39,10 +41,11 @@ const options = readonly({
     </div>
     <div>
       <label for="dates" class="block">Dates</label>
-      <select id="dates" v-model="modelDate" name="Dates" class="p-1 bg-dark-bg-secondary">
+      <select id="dates" v-model.number="modelDate" name="Dates" class="p-1 bg-dark-bg-secondary">
         <option disabled value="">Please select one...</option>
-        <option v-for="date in options.dates" :key="date" :value="date">
-          {{ date }}
+        <option :value="undefined">All</option>
+        <option v-for="month, index in $tm('message.months')" :key="month" :value="index">
+          {{ month }}
         </option>
       </select>
     </div>
