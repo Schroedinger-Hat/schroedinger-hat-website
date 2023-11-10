a06e311a45ad675d544352e521fe18e63a225d69
Patrick Raedler <code@readpato.dev>
2023-11-10 23:50:39 +0100

    refactor(EventForm): utilizer new EventForm component

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, readonly } from 'vue'
 import type { EventData } from '@/i18n/events/index'
+import EventFormSelect from '@/components/EventFormSelect.vue'
 
 const props = defineProps<{
   events: EventData[]
@@ -13,41 +14,17 @@ const modelDate = defineModel<number>('modelDate')
 const options = readonly({
   categories: computed(() => new Set(props.events.map(event => event.category))),
   cities: computed(() => new Set(props.events.map(event => event.location.city))),
-  dates: computed(() => new Set(props.events.map(event => event.date.day))),
 })
 </script>
 
 <template>
-  <form class="flex justify-evenly items-center mb-8">
-    <div>
-      <label for="cities" class="block">Location</label>
-      <select id="cities" v-model="modelCity" name="Cities" class="p-1 bg-dark-bg-secondary rounded-lg">
-        <option disabled value="">Please select one...</option>
-        <option :value="undefined">All</option>
-        <option v-for="city in options.cities" :key="city" :value="city">
-          {{ city }}
-        </option>
-      </select>
-    </div>
-    <div>
-      <label for="categories" class="block">Category</label>
-      <select id="categories" v-model="modelCategory" name="Categories" class="capitalize p-1 bg-dark-bg-secondary">
-        <option disabled value="">Please select one...</option>
-        <option :value="undefined">All</option>
-        <option v-for="category in options.categories" :key="category" :value="category">
-          {{ category }}
-        </option>
-      </select>
-    </div>
-    <div>
-      <label for="dates" class="block">Dates</label>
-      <select id="dates" v-model.number="modelDate" name="Dates" class="p-1 bg-dark-bg-secondary">
-        <option disabled value="">Please select one...</option>
-        <option :value="undefined">All</option>
-        <option v-for="month, index in $tm('message.months')" :key="month" :value="index">
-          {{ month }}
-        </option>
-      </select>
-    </div>
+  <form class="flex flex-col lg:flex-row justify-evenly items-center mb-8">
+    <EventFormSelect v-model:model="modelCity" :items="options.cities" item-name="Location" />
+    <EventFormSelect v-model:model="modelCategory" :items="options.categories" item-name="Categories" />
+    <EventFormSelect v-model:model="modelDate" item-name="Dates">
+      <option v-for="month, index in $tm('message.months')" :key="month" :value="Number(index)">
+        {{ month }}
+      </option>
+    </EventFormSelect>
   </form>
 </template>
