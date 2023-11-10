e149ad447f075a98d14c8f837f188e7303bc9de2
Patrick Raedler <code@readpato.dev>
2023-11-10 23:50:30 +0100

    feat(EventFormSelect): create select component

---
+++ src/components/EventFormSelect.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  items?: ReadonlySet<string>
+  itemName: string
+}>()
+
+const model = defineModel<string | number>('model')
+</script>
+
+<template>
+  <div class="min-w-40">
+    <label for="cities" class="block w-full">{{ itemName }}</label>
+    <select id="cities" v-model="model" name="Cities" class="w-full p-1 bg-dark-bg-secondary rounded-lg capitalize">
+      <option disabled value="">Select one</option>
+      <option :value="undefined">All</option>
+      <slot>
+        <option v-for="item in items" :key="item" :value="item">
+          {{ item }}
+        </option>
+      </slot>
+    </select>
+  </div>
+</template>
