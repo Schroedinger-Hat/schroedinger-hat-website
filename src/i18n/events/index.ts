1ead5bd4afd2f5ca267e5e551b65ca0b5f6b7424
Patrick Raedler <code@readpato.dev>
2023-11-12 18:21:30 +0100

    refactor(i18n/events): update events structure and type

--- src/i18n/events/index.ts
+++ src/i18n/events/index.ts
@@ -1,5 +1,6 @@
 import OSDay2021 from '@/i18n/events/event/osday-21.json'
 import OSDay2023 from '@/i18n/events/event/osday-23.json'
+import OSDay2024 from '@/i18n/events/event/osday-24.json'
 import Qwik0623 from '@/i18n/events/session/qwik-06-23.json'
 import Qwik0624 from '@/i18n/events/session/qwik-06-24.json'
 import Page from '@/i18n/events/page.json'
@@ -21,7 +22,7 @@ interface Location {
 }
 
 interface Schedule {
-  day: string
+  date: string
   start: string
   end: string
 }
@@ -35,7 +36,7 @@ export interface Event {
   id: string
   category: string
   featured: boolean
-  headline?: string
+  headline: string | null
   ticketsURL: string
   title: string
   details: Detail[]
@@ -67,7 +68,7 @@ const groupByLanguage = <T>(items: LanguageItem<T>[]) => {
   return group
 }
 
-const eventsJSON: EventJSON[] = [OSDay2021, OSDay2023, Qwik0623, Qwik0624]
+const eventsJSON: EventJSON[] = [OSDay2021, OSDay2023, Qwik0623, Qwik0624, OSDay2024]
 
 export const messages = {
   data: groupByLanguage(eventsJSON),
