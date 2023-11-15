cf8cb7990bf4a795da0232c112da961c1762f639
Patrick Raedler <code@readpato.dev>
2023-11-15 23:59:25 +0100

    refactor(useNewsletter): create variable for newsletter id

--- src/functions/useNewsletter.ts
+++ src/functions/useNewsletter.ts
@@ -1,14 +1,16 @@
+const NEWSLETTER_ID = 'e9fb84c2-ebfc-11ed-8424-fbce0ce9d7bc'
+
 export const useNewsletter = () => {
   const createScript = () => {
     const $script = document.createElement('script')
-    $script.setAttribute('src', 'https://eocampaign1.com/form/e9fb84c2-ebfc-11ed-8424-fbce0ce9d7bc.js')
+    $script.setAttribute('src', `https://eocampaign1.com/form/${NEWSLETTER_ID}.js`)
     $script.setAttribute('async', '')
-    $script.setAttribute('data-form', 'e9fb84c2-ebfc-11ed-8424-fbce0ce9d7bc')
+    $script.setAttribute('data-form', NEWSLETTER_ID)
     document.head.appendChild($script)
   }
 
   const init = () => {
-    if (!document.cookie.includes('e9fb84c2-ebfc-11ed-8424-fbce0ce9d7bc'))
+    if (!document.cookie.includes(NEWSLETTER_ID))
       createScript()
   }
 
