eedfdcb5c3b3507100caf7597a9fed382a574014
Patrick Raedler <code@readpato.dev>
2023-11-24 15:16:58 +0100

    refactor: update vite alias and ts paths

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,10 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@functions': fileURLToPath(new URL('./src/functions', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
       // See https://github.com/intlify/vue-i18n-next/issues/789
       'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
     },
