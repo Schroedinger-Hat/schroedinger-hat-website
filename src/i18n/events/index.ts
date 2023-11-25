26733a9cd0823def5ee2efc6f85fcec3eaaa74c1
Patrick Raedler <code@readpato.dev>
2023-11-25 18:23:28 +0100

    chore: create rust session

--- src/i18n/events/index.ts
+++ src/i18n/events/index.ts
@@ -2,6 +2,7 @@ import OSDay2021 from '@/i18n/events/event/osday-21.json'
 import OSDay2023 from '@/i18n/events/event/osday-23.json'
 import OSDay2024 from '@/i18n/events/event/osday-24.json'
 import Qwik0623 from '@/i18n/events/session/qwik-06-23.json'
+import Rust2023 from '@/i18n/events/session/rust-11-23.json'
 import Page from '@/i18n/events/page.json'
 
 interface Detail {
@@ -67,7 +68,7 @@ const groupByLanguage = <T>(items: LanguageItem<T>[]) => {
   return group
 }
 
-const eventsJSON: EventJSON[] = [OSDay2021, OSDay2023, Qwik0623, OSDay2024]
+const eventsJSON: EventJSON[] = [OSDay2021, OSDay2023, Qwik0623, OSDay2024, Rust2023]
 
 export const messages = {
   data: groupByLanguage(eventsJSON),
