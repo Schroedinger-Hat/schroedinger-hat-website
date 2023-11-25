802ce568ac3460ef1b610dc8ad03a3173a2b2fd7
Patrick Raedler <code@readpato.dev>
2023-11-25 19:04:11 +0100

    chore: create verona#1 session

--- src/i18n/events/index.ts
+++ src/i18n/events/index.ts
@@ -3,6 +3,7 @@ import OSDay2023 from '@/i18n/events/event/osday-23.json'
 import OSDay2024 from '@/i18n/events/event/osday-24.json'
 import Qwik0623 from '@/i18n/events/session/qwik-06-23.json'
 import Rust2023 from '@/i18n/events/session/rust-11-23.json'
+import Verona123 from '@/i18n/events/session/verona1-23.json'
 import Page from '@/i18n/events/page.json'
 
 interface Detail {
@@ -68,7 +69,7 @@ const groupByLanguage = <T>(items: LanguageItem<T>[]) => {
   return group
 }
 
-const eventsJSON: EventJSON[] = [OSDay2021, OSDay2023, Qwik0623, OSDay2024, Rust2023]
+const eventsJSON: EventJSON[] = [OSDay2021, OSDay2023, Qwik0623, OSDay2024, Rust2023, Verona123]
 
 export const messages = {
   data: groupByLanguage(eventsJSON),
