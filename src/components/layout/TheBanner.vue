d5a89336ccc8994af02dc16dce6da135c23d0465
Patrick Raedler <code@readpato.dev>
2023-12-17 16:09:20 +0100

    refactor(TheBanner): implement i18n

--- src/components/layout/TheBanner.vue
+++ src/components/layout/TheBanner.vue
@@ -10,8 +10,8 @@ const { banner, handleDismiss } = useBannerCookie()
   <div v-if="!banner.dismissed" class="banner" fixed inset-x-0 bottom-0 p-4 text-center text-base>
     <a :href="OSDAY_URL" target="_blank" flex justify-center items-center>
       <div flex-grow>
-        <p inline>Are you ready for the next edition of <b>OSDay</b>? </p>
-        <p inline-block md:inline>Get your free ticket now! <span inline> &#128640;</span></p>
+        <p inline>{{ $t('main.banner.first') }} <b>OSDay</b>? </p>
+        <p inline-block lg:inline>{{ $t('main.banner.second') }}<span inline> &#128640;</span></p>
       </div>
       <button p-2 text-xl @click.prevent="handleDismiss">
         <Icon icon="carbon:close" />
@@ -22,6 +22,7 @@ const { banner, handleDismiss } = useBannerCookie()
 
 <style scoped lang="scss">
 .banner {
+  z-index: $z-banner;
   background: linear-gradient(90deg, rgb(238 0 0 / 100%) 0%, rgb(190 30 45 / 100%) 100%) !important;
 }
 </style>
