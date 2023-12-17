7940099e5528fe7362874bc1df77dd8084fb2995
Patrick Raedler <code@readpato.dev>
2023-12-17 14:10:53 +0100

    refactor(TheBanner): create placeholder component

---
+++ src/components/layout/TheBanner.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="banner" fixed inset-x-0 bottom-0 p-4 text-center text-base>
+    Are you ready for the next edition of:
+    <b>OSDay</b>?
+    Get your free ticket now!
+    &#128640;
+  </div>
+  <a>
+    Share ticket
+  </a>
+</template>
+
+<style scoped lang="scss">
+.banner {
+  background: linear-gradient(90deg, rgb(238 0 0 / 100%) 0%, rgb(190 30 45 / 100%) 100%) !important;
+}
+</style>
