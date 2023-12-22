e92b9a3012d9c6b8f7a9b379476c4f9e348ed8bd
Patrick Raedler <code@readpato.dev>
2023-12-22 16:55:42 +0100

    refactor(vite.config): allow usage of scss maps

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 const sassAdditionalData = () => {
   let additionalData = '@use "sass:math";'
+  additionalData += '@use "sass:map";'
   additionalData += '@import "@/styles/global";'
   return additionalData
 }
