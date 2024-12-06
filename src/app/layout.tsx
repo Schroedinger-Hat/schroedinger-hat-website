dd0afeaadd265b164e9059577a02ade6ca405d68
lobetia <lobetia@gmail.com>
2024-12-06 12:20:52 +0100

    GA is optional

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,43 @@
-import "@/styles/globals.css"
-import { Inter, Lexend } from "next/font/google"
-import { TRPCReactProvider } from "@/trpc/react"
-import { constructMetadata } from "@/lib/utils/metadata"
-import { GoogleAnalytics } from "@next/third-parties/google"
-import { env } from "@/env.js"
+import "@/styles/globals.css";
+import { Inter, Lexend } from "next/font/google";
+import { TRPCReactProvider } from "@/trpc/react";
+import { constructMetadata } from "@/lib/utils/metadata";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { env } from "@/env.js";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
-const lexend = Lexend({ subsets: ["latin"], variable: "--font-lexend" })
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const lexend = Lexend({ subsets: ["latin"], variable: "--font-lexend" });
 
-export const metadata = constructMetadata()
+export const metadata = constructMetadata();
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${inter.variable} ${lexend.variable}`}>
       <head>
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
         <meta name="apple-mobile-web-app-title" content="SH" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
-      <GoogleAnalytics gaId={env.NEXT_PUBLIC_GA_ID} />
+      {env.NEXT_PUBLIC_GA_ID && (
+        <GoogleAnalytics gaId={env.NEXT_PUBLIC_GA_ID} />
+      )}
     </html>
-  )
+  );
 }
