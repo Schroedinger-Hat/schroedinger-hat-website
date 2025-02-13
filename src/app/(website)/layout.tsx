324fdcf147803ee04afd86a6ce45666d4414ab9d
lobetia <lobetia@gmail.com>
2025-02-13 19:21:06 +0100

    Progress

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,9 +1,7 @@
-import "@/styles/globals.css"
-
 import { Header } from "@/components/organisms/header/header"
 import { Footer } from "@/components/organisms/footer"
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function WebsiteLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
       <Header />
