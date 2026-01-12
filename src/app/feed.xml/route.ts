4e66583e7a713e7b63b22e6a33796fcadec2a4c2
Mattia Lobertini <mattia.lobertini@datadoghq.com>
2026-01-12 21:27:42 +0100

    Fixed url

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -23,7 +23,7 @@ export async function GET() {
     }`,
   )
 
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "https://schrodinger-hat.it"
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "https://schroedinger-hat.org"
 
   const rss = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
